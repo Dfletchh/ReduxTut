6ad140617b2cbc25cc0b913750d4b698ea20d657
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-26 15:12:02 -0500

    Counter w/ buttons created for initial intro

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+// container to store all state
+
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
