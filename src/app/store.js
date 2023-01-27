7b96ea4a28d33caf2f495fa6b68c9284ca2bd697
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-27 10:30:17 -0500

    Commented out counter logic and moved css styles

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,11 @@
 // container to store all state
-
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+/* Counter Logic */
+// import counterReducer from "../features/counter/counterSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
